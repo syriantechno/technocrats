3bc4e6c9c559828f8b223b18dd582e1d286e1789
LOAY <loay@LOAYs-iMac.local>
2019-08-23 04:05:08 +0300

    technocrats lesson

---
+++ index.php
@@ -0,0 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loay
+ * Date: 8/17/19
+ * Time: 9:35 PM
+ */
+$te = "technocrats<br>";
+echo $te;
+$x = 5;
+$y = 10;
+$sum = $x + $y;
+echo $sum;
+?>
+<br><input type="text">  <label>username</label> </br>
+<br><input type="password">  <label>password</label> </br>
+
+<input type="submit" value="تسجيل دخول"  /><br />
